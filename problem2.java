c6e2fbc56735cf1726660d9cfbb622d37ff53bf2
unknown <anish.tron@gmail.com>
2021-09-21 21:44:20 +0100

    problem 2 solution

---
+++ problem2.java
@@ -0,0 +1,23 @@
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int value = 0;
+        int total = 0;
+
+        while (secondNumber < 4000000) {
+            value = firstNumber + secondNumber;
+
+            if (value % 2 == 0) {
+                total += value;
+            }
+
+            firstNumber = secondNumber;
+            secondNumber = value;
+        }
+
+        System.out.println(total);
+    }
+}
